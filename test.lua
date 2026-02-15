5b33e098b9a890a4218aa1955b4a528868beb6d5
andreythedevv <andreythedev2@gmail.com>
2026-02-16 03:08:05 +0500

    i think works it or no

--- test.lua
+++ test.lua
@@ -8,6 +8,8 @@
     // also credits to verified (i stole his idea lmao)
     https://scriptblox.com/script/Universal-Script-Change-output-color-16903
 ]]
+local LogService = game:GetService("LogService")
+
 local BetterPrint = {
     Colors = {
         ["Default"] = "255,255,255",
@@ -29,52 +31,49 @@ local BetterPrint = {
 --// helpers
 local function MakeTextRich() --// YAY TEXT NOW IS SUPER RICH WHATAFU
     game:GetService("RunService").Heartbeat:Connect(function()
-      if game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster") then 
-          for _, t in pairs(game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):GetDescendants()) do 
-              if t:IsA("TextLabel") then 
-                  t.RichText = true 
-              end 
-          end 
-      end
-      pcall(function() --// its WILL throw errors cuz im dumbass (were just throwing this errors away)
-        local consoleview = game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):FindFirstChild("DevConsoleWindow"):FindFirstChild("DevConsoleUI"):FindFirstChild("MainView"):FindFirstChild("ClientLog")
-        if not consoleview then
-            return
-        end
-        if BetterPrint.Container ~= nil then  --// also this code is sh!t
-            for _, t in pairs(BetterPrint.Container:GetDescendants()) do 
-                if t then 
-                    local n = t.Name
-
-                    for index,label in pairs(game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):GetDescendants()) do 
-                        if label:IsA("TextLabel") and string.find(label.Text:lower(),n:lower()) then 
-                            labelforimg = label 
-                            if labelforimg.Parent:FindFirstChild(n) then
-                                return
-                            end
-                            c = t:Clone()
-                            c.Parent = labelforimg.Parent
-                            c.Visible = true
-                            break
-                        end 
+      pcall(function()
+        if game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster") then 
+            for _, t in pairs(game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):GetDescendants()) do 
+                if t:IsA("TextLabel") then 
+                    if t.RichText ~= true then
+                        t.RichText = true 
                     end
                 end 
             end 
-        end
+        end          
       end)
+
+    pcall(function()
+        local consoleview = game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):FindFirstChild("DevConsoleWindow"):FindFirstChild("DevConsoleUI"):FindFirstChild("MainView"):FindFirstChild("ClientLog")
+        if not consoleview or not BetterPrint or not BetterPrint.Container then return end
+        
+        for _, label in pairs(game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):GetDescendants()) do
+            if label:IsA("TextLabel") then
+                for _, img in pairs(BetterPrint.Container:GetDescendants()) do
+                    if img:IsA("ImageLabel") and string.find(label.Text:lower(), img.Name:lower(), 1, true) and not label.Parent:FindFirstChild(img.Name) then
+                        c = img:Clone()
+                        c.Parent = label.Parent
+                        c.Visible = true
+                    end
+                end
+            end
+        end
+     end)
     end)
 end
 local function setupEnv()
-    if game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):FindFirstChild("BetterPrint") then
-        BetterPrint.Container = game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):FindFirstChild("BetterPrint")
-        return
-    end
+    pcall(function() --// also throwing errs so i just throwing it away :3
+        if game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):FindFirstChild("BetterPrint") then
+            BetterPrint.Container = game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):FindFirstChild("BetterPrint")
+            return
+        end
 
-    local container = Instance.new("Folder")
-    container.Name = "BetterPrint"
-    container.Parent = game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster")
+        local container = Instance.new("Folder")
+        container.Name = "BetterPrint"
+        container.Parent = game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster")
 
-    BetterPrint.Container = container
+        BetterPrint.Container = container        
+    end)
 end
 local function IsStartsWithNum(str)
     if str:match("^%d") then
@@ -95,7 +94,7 @@ local function generateAscii(text, font) --// we use https://asciified.thelicato
 end
 
 --// functions
-function BetterPrint:print(text, color, size)
+function BetterPrint.print(text, color, size)
     if BetterPrint.Initiliazed ~= true then 
         print("use BetterPrint:Init() before call this!") 
         return
@@ -114,7 +113,7 @@ function BetterPrint:print(text, color, size)
     formatted = formatted .. '>' .. tostring(text) .. '</font>'
     print(formatted)
 end
-function BetterPrint:Loading(title, completeText, color, wait, symbol) --// credits to Verified (https://scriptblox.com/script/Universal-Script-Luarmor-like-loading-ig-18152)
+function BetterPrint.Loading(title, completeText, color, wait, symbol) --// credits to Verified (https://scriptblox.com/script/Universal-Script-Luarmor-like-loading-ig-18152)
     if BetterPrint.Initiliazed ~= true then 
         print("use BetterPrint:Init() before call this!") 
         return
@@ -156,7 +155,7 @@ function BetterPrint:Loading(title, completeText, color, wait, symbol) --// cred
     end
     loadingLabel.Text = string.format("<font color='rgb(%s)' size='15'>[%s] %s</font>", color, title, completeText)
 end
-function BetterPrint:printimage(assetid, scaletype, height, width)
+function BetterPrint.printimage(assetid, height, width, scaletype)
     if BetterPrint.Initiliazed ~= true then 
         print("use BetterPrint:Init() before call this!") 
         return
@@ -168,7 +167,7 @@ function BetterPrint:printimage(assetid, scaletype, height, width)
     id = tostring(math.random(0, 10000000))
     print(" ".. id) --// for replacing
 
-    for i = 0, math.ceil(height / 19) do --// filling gaps (FIXME: fix bug that makes prints that we DONT NEED)
+    for i = 0, math.ceil(height / 20) do --// filling gaps (FIXME: fix bug that makes prints that we DONT NEED)
         print()
     end
 
@@ -186,11 +185,11 @@ function BetterPrint:printimage(assetid, scaletype, height, width)
     image.ScaleType = scaletype
     image.Visible = false
 end
-function BetterPrint:printascii(text, color, font)
-    if not request then 
-        BetterPrint.print("ur exploit didnt support request so we cant generate ascii now :(")
+function BetterPrint.printascii(text, color, font)
+    if not request then
+        BetterPrint.print("ur exploit didnt supports request funcs so we cant generate ascii")
+        return
     end
-
     text = text or "BetterPrint!"
     font = font or "Standard"
     color = color or "Default"
@@ -200,18 +199,14 @@ function BetterPrint:printascii(text, color, font)
         color = BetterPrint.Colors[color]
     end
 
-    BetterPrint.print(generateAscii("BetterPrint!", "Standard"), color)
+    BetterPrint.print(generateAscii(text, "Standard"), color)
 end
-
-function BetterPrint:Examples()
+function BetterPrint.what()
     if BetterPrint.Initiliazed ~= true then 
         print("use BetterPrint:Init() before call this!") 
         return
     end
-    BetterPrint.print("github.com/andreythedev/BetterPrint/examples", "Default")
-end
-function BetterPrint:Clear() --// TODO: remake ts sh!t func
-    print(("\n"):rep(50)) --// ts is best what i can make now 
+    BetterPrint.printimage("rbxassetid://85702299740614", 200, 200)
 end
 
 --// System
@@ -220,6 +215,9 @@ function BetterPrint:GetVersion()
 end
 function BetterPrint:GetColors()
     return BetterPrint.Colors
+end
+function BetterPrint:Clear() --// TODO: remake ts sh!t func
+    print(("\n"):rep(50)) --// ts is best what i can make now 
 end
 function BetterPrint:Init(ShowCredits)
     ShowCredits = ShowCredits or false --// please dont disable it :(
@@ -234,5 +232,3 @@ function BetterPrint:Init(ShowCredits)
 
     BetterPrint.Initiliazed = true
 end
-
-return BetterPrint
